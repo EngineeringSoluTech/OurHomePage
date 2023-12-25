7b9ac74a3695f7a603f4b7d3cc83c192c8b2832b
EngineeringSoluTech <engineringsolutech@gmail.com>
2023-12-25 13:30:56 -0500

    click out sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,40 +1,61 @@
-import "./sidebar.scss"
-import {motion} from "framer-motion"
-import Links from "./links/Links"
-import ToggleButton from "./toggleButton/ToggleButtons"
-import { useState } from "react"
+import React, { useState, useEffect, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+import Links from "./links/Links";
+import ToggleButton from "./toggleButton/ToggleButtons";
+import "./sidebar.scss";
 
 function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const controls = useAnimation();
+  const sidebarRef = useRef(null);
 
-  const [open, setOpen] = useState(false)
-
-  const variants ={
+  const variants = {
     open: {
-      clipPath:"circle(1200px at 50px 50px)",
-      transition:{
-        type:"spring",
-        
-      }
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+      },
     },
     closed: {
-      clipPath:"circle(30px at 50px 50px)",
-      transition:{
-        delay:0.5,
-        type:"spring",
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
         stiffness: 400,
-        damping:40
-      }
+        damping: 40,
+      },
+    },
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setOpen(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    controls.start(open ? "open" : "closed");
+  }, [open, controls]);
 
   return (
-    <motion.div className='sidebar' animate={open ? "open":"closed"}>
+    <motion.div className="sidebar" animate={controls} ref={sidebarRef}>
       <motion.div className="bg" variants={variants}>
-        <Links/>
+        <Links />
       </motion.div>
-      <ToggleButton setOpen={setOpen}/>
+      <ToggleButton setOpen={toggleSidebar} />
     </motion.div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
